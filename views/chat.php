22e85f8a8016dadb4b5ba3aa4ba685130ca45c3a
Dadis <139597407+D4dis@users.noreply.github.com>
2024-11-12 09:56:26 +0100

    Update chat

--- views/chat.php
+++ views/chat.php
@@ -38,7 +38,7 @@ require 'inc/headChat.php';
           <form action="" method="post" id="chatForm">
             <div class="card-footer d-flex align-items-center gap-2 border-top border-translucent ps-3 pe-4 py-3">
               <div class="d-flex align-items-center flex-1 gap-3 border border-translucent rounded-pill px-4">
-                <input class="form-control outline-none border-0 flex-1 fs-9 px-0" type="text" name="message" id="inputMsg" placeholder="Write message" autocomplete="off">
+                <input class="form-control outline-none border-0 flex-1 fs-9 px-0" type="text" name="message" id="inputMsg" placeholder="Entrer votre message" autocomplete="off">
               </div>
               <button class="btn p-0 border-0 send-btn" type="submit"><svg class="svg-inline--fa fa-paper-plane fs-9" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="paper-plane" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
                   <path fill="currentColor" d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"></path>
