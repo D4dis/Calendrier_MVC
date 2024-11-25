ae16376ae15edc5ceef1b85839bd8bd68b275ad9
Dadis <139597407+D4dis@users.noreply.github.com>
2024-11-25 10:18:08 +0100

    id error for api

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -5,15 +5,23 @@ class ApiController
   public function index()
   {
     $model = new ApiModel();
+    if(!isset($_GET['id']) || empty($_GET['id'])){
+      require 'views/404.php';
+    }
     $userDatas = $model->getUserById($_GET['id']);
-    $caserneDatas = $model->getCaserneById($_GET['id']);
-    $uniteDatas = $model->getUniteById($_GET['id']);
-    $caserne = new Caserne($caserneDatas);
-    $unite = new Unite($uniteDatas);
-    $user = new User($userDatas, $caserne);
-    $user->caserne = $caserne;
-    $user->unite = $unite;
-    $json = json_encode($user);
-    require 'views/api.php';
+    if(empty($userDatas)){
+      $error = 'Veuillez entrer un ID correct';
+      require 'views/404.php';
+    } else {
+      $caserneDatas = $model->getCaserneById($_GET['id']);
+      $uniteDatas = $model->getUniteById($_GET['id']);
+      $caserne = new Caserne($caserneDatas);
+      $unite = new Unite($uniteDatas);
+      $user = new User($userDatas, $caserne);
+      $user->caserne = $caserne;
+      $user->unite = $unite;
+      $json = json_encode($user);
+      require 'views/api.php';
+    }
   }
 }
